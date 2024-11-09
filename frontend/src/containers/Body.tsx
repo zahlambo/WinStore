491b69ac440f32905872abec61b48c48f2be1206
James Zaman <james.hedayet@gmail.com>
2024-11-09 23:19:32 +0600

    Now we are showing cards properly

--- frontend/src/containers/Body.tsx
+++ frontend/src/containers/Body.tsx
@@ -1,13 +1,41 @@
-import Card from "../components/Card"
+import { useEffect, useState } from "react";
+import Card from "../components/Card";
 
-function Body() {
-  return (
-    <div className="flex px-16 gap-8">
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  )
+interface AppData {
+  id: string;
+  name: string;
+  icon: string;
 }
 
-export default Body
+function Body() {
+  const backendMainURL = import.meta.env.VITE_BACKEND_MAIN_URL as string;
+  const [allApps, setAllApps] = useState<AppData[]>([]);
+  
+  const searchData = async (params?: string) => {
+    let data: AppData[] = [];
+    try {
+      const response = await fetch(
+        params 
+          ? `${backendMainURL}/get_all_items?search=${params}`
+          : `${backendMainURL}/get_all_items`
+      );
+      data = await response.json();
+      console.log("Data fetched:", data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+    setAllApps(data);
+  };
+
+  useEffect(() => {searchData();}, []);
+
+  return (
+    <div className="flex px-16 gap-8">
+      {allApps.map(app => (
+        <Card key={app.id} id={app.id} name={app.name} icon={app.icon.replace(/width="[^"]+"/, 'width="75%"').replace(/height="[^"]+"/, 'height="75%"')} />
+      ))}
+    </div>
+  );
+}
+
+export default Body;
