c186ecd53da1df28de86cc3a58a1a82eb21abfa3
zahlambo <zahlambo@gmail.com>
2024-11-09 21:39:37 +0600

    fixed connection issue

--- backend/db.py
+++ backend/db.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get MongoDB credentials from environment variables
-MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URL = os.getenv("MONGO_URL")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
 
-client = AsyncIOMotorClient(MONGO_URI)
+client = AsyncIOMotorClient(MONGO_URL)
 db = client[MONGO_DB_NAME]
 
 def get_database():
