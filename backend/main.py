b2ae81fb0166db6a75e970d2cca25c29ff04d0c1
zahlambo <zahlambo@gmail.com>
2024-11-10 00:07:41 +0600

    used cammel case formating

--- backend/main.py
+++ backend/main.py
@@ -3,32 +3,40 @@ from db import get_database
 import json
 from model import *
 import re
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 db = get_database()
 collection = db["apps"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[""],
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
 
-@app.post("/add_item", response_model=str)
-async def add_item(item: AddItem):
-    # Insert the new item into the database
-    await collection.insert_one(item.model_dump())
+
+@app.post("/addItem", response_model=str)
+async def addItem(item: AddItem):
+    await collection.insert_one({"name": item.name, "icon": item.icon, "id": item.id})
     return "Item added successfully!"
 
 
-@app.get("/get_all_items")
-async def get_all_items():
+@app.get("/getAllItems")
+async def getAllItems():
     items = await collection.find({}, {"_id": 0}).to_list()
     return items
 
 
-@app.get("/items/{appquery}", response_model=str)
-async def read_item(items: str):
-    winget_script = "winget install "
-    for i in items.split(","):
+@app.get("/items/{appQuery}", response_model=str)
+async def readItem(appQuery: str):
+    wingetScript = "winget install "
+    for itemName in appQuery.split(","):
         # Removing spaces from the string
-        i = i.strip().replace(" ", "")
-        item = await collection.find_one({"name": re.compile(i, re.IGNORECASE)})
+        itemName = itemName.strip().replace(" ", "")
+        item = await collection.find_one({"name": re.compile(itemName, re.IGNORECASE)})
         if item:
-            winget_script += item['ID'] + " "
-    return winget_script
+            wingetScript += item['id'] + " "
+    return wingetScript
--- backend/model.py
+++ backend/model.py
@@ -3,5 +3,5 @@ from typing import Optional
 
 class AddItem(BaseModel):
     name: str
-    Icon: str=Field(description="icon path actually",alias='icon')
-    ID: str
+    icon: str
+    id: str
