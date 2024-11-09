05c7fb4d17ad83e237cf7c940f09dc75d452a196
zahlambo <zahlambo@gmail.com>
2024-11-10 01:51:25 +0600

    added delete

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException,Depends
+from fastapi import FastAPI, HTTPException, Depends
 from db import get_database
 import json
 from model import *
@@ -12,65 +12,77 @@ collection = db["apps"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[""],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=[""],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
 @app.put("/updateItem", response_model=str)
-def updateItem(name: Optional[str] = None,id: Optional[str] = None, item: AddItem = Depends(AddItem)):
-    query = {}
-    if name:
-        query["name"] = name
-    elif id:
-        query["id"] = id
+async def update_item(appid: str, item: AddItem = Depends(AddItem)):
+    # Check if an item with the same ID already exists
+    existing_item = await collection.find_one({"id": appid})
+    if existing_item:
+        await collection.update_one(
+            {"id": appid},
+            {"$set": {"name": item.name, "icon": item.icon, "id": item.id}}
+        )
     else:
-        raise HTTPException(status_code=400, detail="Name or ID must be provided")
+        raise HTTPException(status_code=404, detail="Item not found")
 
-    items = collection.update_one(query)
-    return items
+    return "Item updated successfully!"
+
+
+@app.delete("/deleteItem", response_model=str)
+async def deleteItem(appid: str):
+    # Check if an item with the same ID already exists
+    existing_item = await collection.find_one({"id": appid})
+    if existing_item:
+        await collection.delete_one({"id": appid})
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    return "Item deleted successfully!"
 
 
 @app.post("/addItem", response_model=str)
-async def addItem(item: AddItem):
+async def addItem(name: str, icon: str, id: str):
     # Check if an item with the same name or ID already exists
-    existing_item = await collection.find_one({"$or": [{"name": item.name}, {"id": item.id}]})
-    
-    if existing_item:
-        raise HTTPException(status_code=400, detail="Item with the same name or ID already exists")
+    existing_item = await collection.find_one({"$or": [{"name": name}, {"id": id}]})
 
-    await collection.insert_one({"name": item.name, "icon": item.icon, "id": item.id})
+    if existing_item:
+        raise HTTPException(
+            status_code=400, detail="Item with the same name or ID already exists")
+
+    await collection.insert_one({"name": name, "icon": icon, "id": id})
     return "Item added successfully!"
 
 
 @app.get("/getAllItems")
-async def getAllItems(name: Optional[str] = None,id: Optional[str] = None):
+async def getAllItems(name: Optional[str] = None, id: Optional[str] = None):
     query = {}
     if name and id:
-        query["$or"] = [{"name": re.compile(name, re.IGNORECASE)}, {"id": re.compile(id, re.IGNORECASE)}]
+        query["$or"] = [{"name": re.compile(name, re.IGNORECASE)}, {
+            "id": re.compile(id, re.IGNORECASE)}]
     elif id:
         query["id"] = re.compile(id, re.IGNORECASE)
 
     elif name:
         query["name"] = re.compile(name, re.IGNORECASE)
-    
 
-    items = await collection.find(query, {"_id": 0,}).to_list()
+    items = await collection.find(query, {"_id": 0, }).to_list()
     return items
 
 
 @app.get("/items/{appQuery}", response_model=str)
 async def readItem(appQuery: str):
     wingetScript = "winget install "
-    itemNames=[ name.strip().replace(" ", "") for name in appQuery.split(",")]
-    items= await collection.find({"name": {"$in": [re.compile(name, re.IGNORECASE) for name in itemNames]}}).to_list()
+    itemNames = [name.strip().replace(" ", "") for name in appQuery.split(",")]
+    items = await collection.find({"name": {"$in": [re.compile(name, re.IGNORECASE) for name in itemNames]}}).to_list()
 
     if items:
         wingetScript += " ".join([item["id"] for item in items])
     else:
         raise HTTPException(status_code=404, detail="Items not found")
     return wingetScript
-
-
